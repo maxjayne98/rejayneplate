2f0363ddd31e4fed7f4193e8d097e435c87c2048
mahdi <mahdi.jax@gmail.com>
2021-01-04 18:13:26 +0330

    remove useless console.log

--- src/redux/shared/actions.js
+++ src/redux/shared/actions.js
@@ -3,7 +3,6 @@ import api from "./api";
 export const increaseCounter = (num) => sharedActions.increment(num);
 export const decreaseCounter = (num) => sharedActions.decrement(num);
 export const fetchUser = () => async (dispatch) => {
-  console.log("req req");
   try {
     await api
       .users(1, "ir")
