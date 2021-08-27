973ca4ede7bd5921a7834c73dbbaccf9161bfe6a
mahdi <mahdi.jax@gmail.com>
2021-08-28 03:14:04 +0430

    feat(base): add css normalizer

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,10 +1,15 @@
 import { createGlobalStyle } from "styled-components";
+import { normalize } from "./normalizer";
+
 export const GlobalStyles = createGlobalStyle`
+  ${normalize}
+
   body {
     background: ${({ theme }) => {
       return theme.body;
     }};
     transition: background 0.2s ease-in, color 0.2s ease-in;
+    direction:rtl;
   }
   html { font-size: 62.5%; }
 
@@ -13,6 +18,7 @@ export const lightTheme = {
   body: "#f1f1f1",
   text: "#121620",
   primary: "#151515",
+  textPrimary: "#969892",
 };
 export const darkTheme = {
   body: "#151515",
@@ -23,4 +29,5 @@ export const darkTheme = {
   textColor3: "#f1f1f1",
   textColor4: "#f1f1f1",
   iconColor1: "#cbcbcb",
+  textPrimary: "#969892",
 };
